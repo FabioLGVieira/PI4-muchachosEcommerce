19e392c5759332539d54d03e150f960dc0c4287d
fabio <vieira.fa@hotmail.com>
2020-10-25 07:16:22 -0300

    adicionando novamente a classe de conexao

---
+++ src/main/java/com/muchachos/DAO/ConnectionDB.java
@@ -0,0 +1,27 @@
+package com.muchachos.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Fabio
+ */
+public class ConnectionDB {
+    
+         public static Connection getConnection()
+            throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+
+        // Passo 1: Registrar driver JDBc
+        Class.forName("com.mysql.jdbc.Driver");
+        // Passo 2: Obter a conexao
+        conn = DriverManager.getConnection(
+                "jdbc:mysql://127.0.0.1:3306/ECOMMERCE_Project",
+                "root",
+                "");
+        return conn;
+         }
+    
+}
